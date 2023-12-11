a11b6b402283054f4415472623ec81239dc2e82c
adamEllisMacBook <125833852+adamEllisMacBook@users.noreply.github.com>
2023-12-11 15:42:31 +0000

    Commit One

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,7 @@
 //  rm -rf .git & .gitignore to the root then rm / node_modules then add .env
 // ls -a to check
 const App = () => {
-  return (
-    <h1>
-      Welcom to prop
-    </h1>
-  )
-}
+  return <h1>Welcom to prop</h1>;
+};
 
-export default App
+export default App;
